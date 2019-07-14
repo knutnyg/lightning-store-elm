79d461778ae77599665e2c6fa3c05d0b898ef8e9
Knut Nygaard <knutnyg@gmail.com>
2019-07-14 19:41:38 +0200

    Fetching node info

--- src/Main.elm
+++ src/Main.elm
@@ -1,47 +1,72 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, div, h1, h2, img, p, text)
+import Http
+import Json.Decode exposing (Decoder, field, string, int, map)
 import Html.Attributes exposing (src)
 
 
 ---- MODEL ----
-
-
 type alias Model =
-    {}
+    {
+        nodeInfo: Maybe NodeInfo
+    }
+
+type alias NodeInfo =
+    {
+        blockHeight: Int
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
-
-
+    ( { nodeInfo = Nothing }, getInfo )
 
 ---- UPDATE ----
-
-
 type Msg
-    = NoOp
+    = GotInfo (Result Http.Error NodeInfo)
+
+getInfo : Cmd Msg
+getInfo =
+    Http.get
+    {
+        url = "http://localhost:8080/status",
+        expect = Http.expectJson GotInfo getInfoDecoder
+    }
+
+getInfoDecoder : Decoder NodeInfo
+getInfoDecoder =
+    map NodeInfo
+        (field "block_height" int)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
-
-
+    case msg of
+        GotInfo result ->
+            case result of
+                Ok ni -> ({ nodeInfo = Just ni}, Cmd.none)
+                Err _ -> (model, Cmd.none)
 
 ---- VIEW ----
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    div [] [
+        nodeInfoView(model.nodeInfo)
         ]
 
-
+nodeInfoView : Maybe NodeInfo -> Html Msg
+nodeInfoView nodeInfo =
+    case nodeInfo of
+        Just ns ->
+           (div []
+            [ h2 [] [ text "nodeinfo" ]
+            , p [] [ text ("BlockHeight: " ++ String.fromInt ns.blockHeight) ]
+            ])
+        Nothing -> div [] []
 
 ---- PROGRAM ----
 
